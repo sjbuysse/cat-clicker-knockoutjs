44beacca001628b168df9784b451eb7a4aea0895
Simon Buysse <buysse.simon@gmail.com>
2017-01-15 18:45:43 +1300

    Add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
-var Cat = function(){
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('http://www.flickr.com/tabby');
-    this.nicknames = ko.observableArray(['Fluff','Daisy','Kaasje']);
+var initialCats = [{
+      clickCount: 0,
+      name: 'Wazza',
+      imgSrc: 'img/22252709_010df3379e_z.jpg',
+      imgAttribution: 'http://www.flickr.com/wazza',
+      nicknames: ['Fluff','Daisy','Kaasje']
+    },{
+      clickCount: 0,
+      name: 'Tabby',
+      imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+      imgAttribution: 'http://www.flickr.com/tabby',
+      nicknames: ['Fluff','Daisy','Kaasje']
+    },{
+      clickCount: 0,
+      name: 'Kinki',
+      imgSrc: 'img/1413379559_412a540d29_z.jpg',
+      imgAttribution: 'http://www.flickr.com/kinki',
+      nicknames: ['Fluff','Daisy','Kaasje']
+    }
+];
+
+var Cat = function(data){
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
     this.level = ko.computed(function(){
       if(this.clickCount() < 10)
         return 'Newborn';
@@ -15,12 +36,20 @@ var Cat = function(){
         return 'adult';
     }, this);
 };
+
 var ViewModel = function(){
     var self = this;
-    this.currentCat = ko.observable(new Cat());
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function(cat){
+      self.catList().push(ko.observable(new Cat(cat)));
+    });
+    this.currentCat = ko.observable(this.catList()[0]);
     this.incrementCounter = function(){
       self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
+    this.setCat = function(cat){
+      self.currentCat(cat);
+    }
 };
 
 ko.applyBindings(new ViewModel());
