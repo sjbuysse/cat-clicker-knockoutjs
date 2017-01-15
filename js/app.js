29e4e1738397e273417f3607ac95965a77cdab32
Simon Buysse <buysse.simon@gmail.com>
2017-01-15 15:59:31 +1300

    Add nicknames to model and present them in the view

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var ViewModel = function(){
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('http://www.flickr.com/tabby');
+    this.nicknames = ko.observableArray(['Fluff','Daisy','Kaasje']);
     this.level = ko.computed(function(){
       if(this.clickCount() < 10)
         return 'Newborn';
